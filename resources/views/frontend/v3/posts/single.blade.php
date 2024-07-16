7d3111921bec781c894729eb761b993f7edc2449
Beslan <64842276+Beslan066@users.noreply.github.com>
2024-07-16 20:03:08 +0300

    Скрыл комментарий с ингушской новости

--- resources/views/frontend/v3/posts/single.blade.php
+++ resources/views/frontend/v3/posts/single.blade.php
@@ -67,30 +67,31 @@
                             <p class="mb-10 text-base font-semibold">{{ $post->lead }}</p>
                         </div>
 
-                        @if(!empty($post->comment))
-                            <div class="post-expert-section flex ">
-                                @if($post->expert && $post->expert->file)
-                                    <div class="expert-img">
-                                        <img src="{{ $post->expert->file->full_preview_path }}" alt="">
-                                    </div>
-                                @endif
-
-                                <div class="comment-body">
-
-                                    @if($post->expert)
-                                        <div class="expert-info">
-                                            <p>{{$post->expert->title}}</p>
-                                            <p>{!! $post->expert->description !!}</p>
+                        @if((App::getlocale() === 'ru'))
+                            @if(!empty($post->comment))
+                                <div class="post-expert-section flex ">
+                                    @if($post->expert && $post->expert->file)
+                                        <div class="expert-img">
+                                            <img src="{{ $post->expert->file->full_preview_path }}" alt="">
                                         </div>
                                     @endif
+
+                                    <div class="comment-body">
+
+                                        @if($post->expert)
+                                            <div class="expert-info">
+                                                <p>{{$post->expert->title}}</p>
+                                                <p>{!! $post->expert->description !!}</p>
+                                            </div>
+                                        @endif
 
                                         <div class="comment">
                                             <p>{{$post->comment}}</p>
                                         </div>
+                                    </div>
                                 </div>
-                            </div>
+                            @endif
                         @endif
-
 
                         <div class="cm-single-description text-base	">
                             {!! $post->description !!}
