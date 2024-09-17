1fe953f9df57b1c82f88e346369621841f049300
Beslan <64842276+Beslan066@users.noreply.github.com>
2024-09-17 10:10:36 +0300

    правильный  вывод автора и описания фотографии

--- resources/views/frontend/v3/posts/single.blade.php
+++ resources/views/frontend/v3/posts/single.blade.php
@@ -52,24 +52,20 @@
                         <div class="">
                             <img class="cm-aspect-4/3 lg:aspect-auto object-cover" src="{{ $post->file->full_preview_path }}" alt="{{$post->title}}">
                         </div>
-                        
+                        @if(isset($post->photo_title))
                             @if(App::getLocale() == 'ru')
-                                @if(isset($post->photo_title))
-                                    <div class="flex flex-row mt-4 justify-between">
-                                        <div class="photo-author ">{{$post->photo_title}}</div>
-                                        <div class="photo-description font-light">{{$post->photo_description}}</div>
-                                    </div>
-                                @endif
+                                <div class="flex flex-row mt-4 justify-between">
+                                    <div class="photo-author ">{{$post->photo_title}}</div>
+                                    <div class="photo-description font-light">{{$post->photo_description}}</div>
+                                </div>
                             @else
-                                @if(isset($post_translation->photo_title))
                                 <div class="flex flex-row mt-4 justify-between">
                                     <div class="photo-author ">{{$post_translation->photo_title}}</div>
                                     <div class="photo-description font-light">{{$post_translation->photo_description}}</div>
                                 </div>
-                                @endif
                             @endif
                         @endif
-                    
+                    @endif
                 </div>
             @endif
 
